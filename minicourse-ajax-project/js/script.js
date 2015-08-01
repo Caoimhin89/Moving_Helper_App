10538d3592c2efc6179e32b6d63b25337c0c8ba1
Kevin <kevin.n.glick@gmail.com>
2015-08-01 16:13:40 +0300

    append articles to page

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -31,6 +31,10 @@ function loadData() {
             $nytElem.append("<li class='article'>" + "<a href='" + article.web_url + "'>" + article.headline.main + "</a>" + "<p>" + article.snippet + "</p>" + "</li>");
         };
         
+        $("<ul id='nytimes-articles'></ul>", {
+            "class": "article";
+            html.items.join("")
+        }).appendTo(body);
         console.log(data);
     });
 
