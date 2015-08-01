6fd1b242feeb1ddb9087f4f2d740c0b2b513b807
Kevin <kevin.n.glick@gmail.com>
2015-08-01 15:20:36 +0300

    nyt api

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -22,6 +22,17 @@ function loadData() {
     $body.append(image);
     // YOUR CODE GOES HERE!
 
+    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=92e51f9adb11f7195f597909588d0ffa:19:72605905"
+    $.getJSON(nytimesUrl, function(data) {
+        $nytHeaderElem.text("New York Times Articles About " + cityStr);
+        articles = data.response.docs;
+        for (var i = 0; i < aricles.length; i++) {
+            $nytElem.append("<li class='article'>" + "<a href='" + article.web_url + "'>" + article.headline.main + "</a>" + "<p>" + article.snippet + "</p>" + "</li>");
+        };
+        
+        console.log(data);
+    });
+
     return false;
 };
 
